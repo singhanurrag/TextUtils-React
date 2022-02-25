8dea29be322d79b5ccba056901ab017a846fb38e
Anurag Singh Bhadauria <anuragsinghbhadauria418@gmail.com>
2022-02-25 22:02:23 +0530

    Updates

---
+++ precache-manifest.4642a0fcfaa73ade8f28b309ee3f65d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c31fb91aee9be6d0ba5d",
+    "url": "/TextUtils-React/static/css/main.8b59c3f7.chunk.css"
+  },
+  {
+    "revision": "c31fb91aee9be6d0ba5d",
+    "url": "/TextUtils-React/static/js/main.c31fb91a.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/TextUtils-React/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils-React/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "3427182f8ed14c1e7cc2",
+    "url": "/TextUtils-React/static/js/runtime~main.3427182f.js"
+  },
+  {
+    "revision": "e68cb1d9f7872a056e6e81d278d215da",
+    "url": "/TextUtils-React/index.html"
+  }
+];
